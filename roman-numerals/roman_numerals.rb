b655c2216ff584720357debff70653f587becc53
doston9471 <doston.7194@gmail.com>
2023-07-08 01:14:22 +0200

    roman numerals

---
+++ roman-numerals/roman_numerals.rb
@@ -0,0 +1,37 @@
+=begin
+Write your code for the 'Roman Numerals' exercise in this file. Make the tests in
+`roman_numerals_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/roman-numerals` directory.
+=end
+
+class Integer
+  ROMAN_NUMERALS = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }.freeze
+
+  def to_roman
+    number = self
+    roman_numeral = ''
+    ROMAN_NUMERALS.each do |value, symbol|
+      while number >= value
+        roman_numeral += symbol
+        number -= value
+      end
+    end
+    roman_numeral
+  end
+end
